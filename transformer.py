b2d4aa597e17aefbb5453dbdc8765d846a374eb5
CarlosEugenio177 <carloseugeniomendes.p.l@gmail.com>
2026-03-15 08:09:52 -0300

    feat: implementação do encoder_block e encoder_stack

--- transformer.py
+++ transformer.py
@@ -3,10 +3,22 @@ from attention import self_attention, add_and_norm, feed_forward, softmax
 from decoder import cross_attention, look_ahead_mask
 
 def encoder_block(X):
-    return X
+
+    X_att = self_attention(X)
+    X_norm1 = add_and_norm(X, X_att)
+
+    X_ffn = feed_forward(X_norm1)
+    X_out = add_and_norm(X_norm1, X_ffn)
+
+    return X_out
 
 def encoder_stack(X, num_layers=6):
+
     Z = X.copy()
+
+    for _ in range(num_layers):
+        Z = encoder_block(Z)
+
     return Z
 
 def decoder_block(Y, Z):
